b1e66b5337d2ed43f8a947a5c5b5eb01fb8f858a
Ridho Ade Putra <ridho.adep@gmail.com>
2020-05-06 19:26:19 +0700

    add internet connection checking

--- app/components/image-post/image-post.styles.ts
+++ app/components/image-post/image-post.styles.ts
@@ -14,7 +14,6 @@ export const imagePostStyles = {
     },
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
-
     elevation: 3,
   } as ViewStyle,
 
--- app/screens/welcome-screen/welcome-screen.tsx
+++ app/screens/welcome-screen/welcome-screen.tsx
@@ -2,11 +2,12 @@ import * as React from "react"
 import { View, ViewStyle, FlatList, RefreshControl } from "react-native"
 import { ParamListBase } from "@react-navigation/native"
 import { NativeStackNavigationProp } from "react-native-screens/native-stack"
-import { Screen, FeedPlaceholder } from "../../components"
-import { color, spacing } from "../../theme"
+import { FeedPlaceholder, Text } from "../../components"
+import {  spacing } from "../../theme"
 import { ImagePost } from "../../components/image-post/image-post"
-import { SearchBar } from 'react-native-elements';
+import { SearchBar } from 'react-native-elements'
 import { Api, FeedResult } from "../../services/api"
+import NetInfo from "@react-native-community/netinfo"
 
 const FULL: ViewStyle = { flex: 1, backgroundColor: '#eee' }
 export interface WelcomeScreenProps {
@@ -16,19 +17,24 @@ export interface WelcomeScreenProps {
 export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = props => {
 
   const [loading, setLoading] = React.useState(true)
-  const [refreshing, ] = React.useState(false)
+  const [refreshing,] = React.useState(false)
   const [data, setData] = React.useState([])
   const [tag, setTag] = React.useState(null)
+  const [connect, setConnect] = React.useState(true)
 
   const api = new Api()
   api.setup()
 
   React.useEffect(() => {
     getData()
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log("Is connected?", state.isConnected);
+      setConnect(state.isConnected)
+    });
   }, [])
 
   async function getData() {
-    console.log("tag:"+tag)
+    console.log("tag:" + tag)
     setLoading(true)
     const res: FeedResult = await api.getFeed(tag)
     setData(res.data)
@@ -36,14 +42,20 @@ export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = props 
   }
 
   function renderList() {
-    return <FlatList
-    refreshControl={
-      <RefreshControl refreshing={refreshing} onRefresh={() => { getData() }} />
-    }
+    return connect ? <FlatList
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={() => { getData() }} />
+      }
       data={data}
-      renderItem={({ item }) => <ImagePost data={item}/>}
+      renderItem={({ item }) => <ImagePost data={item} />}
       keyExtractor={item => item.link}
-    />
+    /> : noInternetView()
+  }
+
+  function noInternetView() {
+    return (
+      <View style={{marginTop:spacing[3]}}><Text>No Internet Connection</Text></View>
+    )
   }
 
   return (
@@ -53,7 +65,8 @@ export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = props 
         onBlur={() => getData()}
         onChangeText={(t) => setTag(t)}
         value={tag}
-        lightTheme style={{ marginBottom: spacing[4] }} />
+        lightTheme
+        style={{ marginBottom: spacing[6] }} />
 
       {loading ? <FeedPlaceholder /> : renderList()}
 
